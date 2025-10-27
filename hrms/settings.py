7a6c3526a137c14eafd74c44162f7884dd3f6f2f
Abhijit Tripathy <abhijittripathy99@gmail.com>
2025-10-27 15:24:35 +0530

    added new changes

--- hrms/settings.py
+++ hrms/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "hrms.urls"
@@ -140,15 +141,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-import os
+
 # settings.py (add)
 # Set these via environment variables in production / docker-compose
 TOGETHER_API_URL = "https://api.together.xyz/v1/chat/completions"
